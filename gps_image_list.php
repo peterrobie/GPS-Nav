ab437649e5e959aab33845813d083bab34d6ea8e
Peter Robie <probie@session-9.peterrobie.com>
2013-09-24 10:55:14 -0400

    Updating paths and cleanup

--- gps_image_list.php
+++ gps_image_list.php
@@ -1,9 +1,9 @@
 <?php
-if ($handle = opendir('./html5fu/uploads')) {
+if ($handle = opendir('./imgUpload')) {
     while (false !== ($entry = readdir($handle))) {
         if ($entry != "." && $entry != "..") {
         	echo '<div class="img_container">';
-        	echo '<img src="/test_bed/html5fu/uploads/' . $entry . '" class="img" onclick="javascript:console.log(\'This is for '. $entry .' \');" exif="true" />';
+        	echo '<img src="' . $entry . '" class="img" onclick="javascript:console.log(\'This is for '. $entry .' \');" exif="true" />';
         	echo '</div>';
         }
     }
